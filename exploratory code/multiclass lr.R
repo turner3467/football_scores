234892d79f0c500d23d38a79ee1c7f8c041e6db9
Jeremy Turner <jturner@retailinsight.co.uk>
2016-08-03 17:08:47 +0100

    First iteration of multiclass lr working.

--- exploratory code/multiclass lr.R
+++ exploratory code/multiclass lr.R
@@ -1,6 +1,8 @@
 setwd("~/Projects/football_scores/exploratory code")
 
-library(mlogit)
+library(tidyr)
+library(dplyr)
+library(glmnet)
 
 measures <- read.csv("../data/processed/complete_measures.csv", stringsAsFactors = FALSE)
 scores.map <- c("A"=-1, "D"=0, "H"=1)
@@ -8,7 +10,10 @@ measures$scores <- scores.map[measures$scores]
 
 measures <- measures[, -c(1:6)]
 
-#measures.imputed <- rfImpute(scores ~ ., data = measures)
+## Dropping head to head columns with large number of missing values
+measures <- measures[, -grep("^hth.*(10|15)$", colnames(measures))]
+measures <- measures[complete.cases(measures), ]
+## measures.imputed <- rfImpute(scores ~ ., data = measures)
 
 smp_size <- floor(0.75 * nrow(measures))
 
@@ -19,12 +24,35 @@ train_ind <- sample(seq_len(nrow(measures)), size = smp_size)
 train <- measures[train_ind, ]
 test <- measures[-train_ind, ]
 
-d <- duplicated(train)
-train.dd <- train[-d, ]
+## Using glmnet
 
-n <- names(train)
-f <- as.formula(paste("scores ~ ", paste(n[!n %in% "scores"], collapse = " + ")))
-ml.fit <- mlogit(f, data = train.dd, shape = "wide", choice = 0)
+y <- as.factor(train$scores)
+x <- as.matrix(select(train, -scores))
 
+lr.fit <- glmnet(x, y, family = "multinomial", alpha = 0, lambda = 0)
 
-## Problem with singular matrix, therefore non invertible
+y.test <- as.factor(test$scores)
+x.test <- as.matrix(select(test, -scores))
+lr.pred <- predict(lr.fit, newx = x.test, s = "lambda.min", type = "response")
+
+## Plotting
+
+library(ggplot2)
+
+tmp <- as.data.frame(lr.pred)
+names(tmp) <- c("L", "D", "W")
+
+tmp2 <- gather(data = tmp, "class", "prob", 1:3)
+ggplot(data = tmp2, aes(x=prob, fill = class, alpha = 0.5)) + geom_density()
+
+## Implement scoring rule
+
+log.scoring.rule <- function(y.hat, y){
+    score <- 0
+    for(idx in seq(1:length(y))){
+        i <- as.character(y[idx])
+        R.i <- y.hat[idx, i, 1]
+        score <- score + log(R.i)
+    }
+    return(score)
+}
